ad96770ba949f8a2e45eb007118acacf9f352c50
MohammadAlHabil <mohammadalhabil1999@gmail.com>
2022-02-13 20:39:45 +0200

    solve the first exercise

--- stage-1/exercise/app.js
+++ stage-1/exercise/app.js
@@ -11,81 +11,113 @@
  * Take your time, and think about what principles you are trying to apply while
  * you are refactoring.
  */
-'use strict';
+"use strict";
 
-document.querySelector('#form-unanswered').addEventListener('submit', function (e) {
+function addEvent(selector, event, callback) {
+  document.querySelector(selector).addEventListener(event, callback);
+}
+
+function createRequest(url, callback) {
   e.preventDefault();
-
   var form = e.target;
-  var tags = form.querySelector('input[name=tags]').value;
-  var url  = 'https://api.stackexchange.com/2.2/questions/unanswered?order=desc&sort=activity&site=stackoverflow&tagged=' + tags;
+  var tag = form.querySelector("input[name=tags]").value;
+  var url = url;
 
   var xhr = new XMLHttpRequest();
-
-  xhr.addEventListener('load', function () {
+  xhr.addEventListener("load", () => {
     if (xhr.status === 200) {
       var response = JSON.parse(xhr.responseText);
-
-      document.querySelector('#results-summary').innerHTML = ''
-        + '<p>'
-        + 'Query of ' + tags +  ' returned ' + response.items.length + ' results'
-        + '</p>';
-
-      document.querySelector('#results-body').innerHTML = response.items.map(function (item) {
-        return ''
-          + '<div>'
-          + '<p>Title: ' + item.title + '</p>'
-          + '<p>Date: ' + new Date(item.creation_date) + '</p>'
-          + '<p>Link: <a href="' + item.link + '">Click here</a></p>'
-          + '<p>Owner: ' + item.owner.display_name + '</p>'
-          + '</div>'
-      })
-      .join('<br>');
-
+      callback();
     } else {
-      console.log('Status Code: ' + xhr.status);
+      console.log("Status Code: " + xhr.status);
     }
   });
 
-  xhr.open('GET', url);
+  xhr.open("GET", url);
   xhr.send();
+}
+
+addEvent("#form-unanswered", "submit", () => {
+  var url =
+    "https://api.stackexchange.com/2.2/questions/unanswered?order=desc&sort=activity&site=stackoverflow&tagged=" +
+    tags;
+  createRequest(url, changeUnanswered);
 });
 
+function changeUnanswered() {
+  document.querySelector("#results-summary").innerHTML =
+    "" +
+    "<p>" +
+    "Query of " +
+    tags +
+    " returned " +
+    response.items.length +
+    " results" +
+    "</p>";
 
-document.querySelector('#form-answerers').addEventListener('submit', function (e) {
-  e.preventDefault();
+  document.querySelector("#results-body").innerHTML = response.items
+    .map(function (item) {
+      return (
+        "" +
+        "<div>" +
+        "<p>Title: " +
+        item.title +
+        "</p>" +
+        "<p>Date: " +
+        new Date(item.creation_date) +
+        "</p>" +
+        '<p>Link: <a href="' +
+        item.link +
+        '">Click here</a></p>' +
+        "<p>Owner: " +
+        item.owner.display_name +
+        "</p>" +
+        "</div>"
+      );
+    })
+    .join("<br>");
+}
 
-  var form = e.target;
-  var tag  = form.querySelector('input[name=tags]').value;
-  var url  = 'http://api.stackexchange.com/2.2/tags/' + tag + '/top-answerers/all_time?site=stackoverflow'
+// --------------------addEvent--------------------------- //
 
-  var xhr = new XMLHttpRequest();
-
-  xhr.addEventListener('load', function () {
-    if (xhr.status === 200) {
-      var response = JSON.parse(xhr.responseText);
-
-      document.querySelector('#results-summary').innerHTML = ''
-        + '<p>'
-        + 'Query of ' + tags +  ' returned ' + response.items.length + ' results'
-        + '</p>';
-
-      document.querySelector('#results-body').innerHTML = response.items.map(function (item) {
-        return ''
-          + '<div>'
-          + '<p>User: ' + item.user.display_name + '</p>'
-          + '<p>Reputation: ' + item.user.reputation + '</p>'
-          + '<p>Profile: <a href="' + item.user.link + '">Click here</a></p>'
-          + '<p>Score: ' + item.score + '</p>'
-          + '</div>'
-      })
-      .join('<br>');
-
-    } else {
-      console.log('Status Code: ' + xhr.status);
-    }
-  });
-
-  xhr.open('GET', url);
-  xhr.send();
+addEvent("#form-answerers", "submit", () => {
+  var url =
+    "http://api.stackexchange.com/2.2/tags/" +
+    tag +
+    "/top-answerers/all_time?site=stackoverflow";
+  createRequest(url, changeAnswerers);
 });
+
+function changeAnswerers() {
+  document.querySelector("#results-summary").innerHTML =
+    "" +
+    "<p>" +
+    "Query of " +
+    tags +
+    " returned " +
+    response.items.length +
+    " results" +
+    "</p>";
+
+  document.querySelector("#results-body").innerHTML = response.items
+    .map(function (item) {
+      return (
+        "" +
+        "<div>" +
+        "<p>User: " +
+        item.user.display_name +
+        "</p>" +
+        "<p>Reputation: " +
+        item.user.reputation +
+        "</p>" +
+        '<p>Profile: <a href="' +
+        item.user.link +
+        '">Click here</a></p>' +
+        "<p>Score: " +
+        item.score +
+        "</p>" +
+        "</div>"
+      );
+    })
+    .join("<br>");
+}
